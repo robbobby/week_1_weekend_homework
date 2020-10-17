613c3f6d1533b27129e04f0f6ca8e8aca4ee517b
Rob <rob@Robs-iMac.local>
2020-10-17 08:34:46 +0100

    Q1 complete

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,4 @@
 # WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop_list):
+    return pet_shop_list["name"]
--- start_point/tests/pet_shop_test.py
+++ start_point/tests/pet_shop_test.py
@@ -76,10 +76,10 @@ class TestPetShop(unittest.TestCase):
         }
 
     def test_pet_shop_name(self):
+        ##### √√√√√ #####
         name = get_pet_shop_name(self.cc_pet_shop)
-        self.assertEqual("Camelot of Pets", name)
+        self.assertEqual("Camelot of Pets", name) 
 
-    @unittest.skip("delete this line to run the test")
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
