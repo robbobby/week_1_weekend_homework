cf0f3028b05c7b17b3b76446045cf51436ef946e
Rob <rob@Robs-iMac.local>
2020-10-17 09:17:06 +0100

    Q3 + Q4 complete

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -7,3 +7,8 @@ def get_pet_shop_name(pet_shop_list):
                             ##### Question 2 #####
 def get_total_cash(pet_shop_list):
     return pet_shop_list["admin"]["total_cash"]
+
+                            ##### Question 3 #####
+                            ##### Question 4 #####
+def add_or_remove_cash(pet_shop_list, value_to_add):
+    pet_shop_list["admin"]["total_cash"] += value_to_add
