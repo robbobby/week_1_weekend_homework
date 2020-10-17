195c7817b1c4c772b50f175880eed762995f92da
Rob <rob@Robs-iMac.local>
2020-10-17 10:50:21 +0100

    Question 7 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -12,3 +12,21 @@ def get_total_cash(pet_shop_list):
                             ##### Question 4 #####
 def add_or_remove_cash(pet_shop_list, value_to_add):
     pet_shop_list["admin"]["total_cash"] += value_to_add
+
+                            ##### Question 5 #####
+def get_pets_sold(pet_shop_list):
+    return pet_shop_list["admin"]["pets_sold"]
+
+                            ##### Question 6 #####
+def increase_pets_sold(pet_shop_list, number_of_pets_sold):
+    pet_shop_list["admin"]["pets_sold"] += number_of_pets_sold
+
+def get_stock_count(pet_shop_list):
+    return len(pet_shop_list["pets"])
+
+def get_pets_by_breed(pet_shop_list, breed_name):
+    pet_list = []
+    for pet in pet_shop_list["pets"]
+        if pet["pet_type"] == breed_name
+        pet_list.append(pet["name"])
+    return pet_list.__len__
--- start_point/tests/pet_shop_test.py
+++ start_point/tests/pet_shop_test.py
@@ -76,43 +76,44 @@ class TestPetShop(unittest.TestCase):
         }
 
     def test_pet_shop_name(self):
-        ##### √√√√√ #####
+        ##### √√√√√ #####       Q1
         name = get_pet_shop_name(self.cc_pet_shop)
         self.assertEqual("Camelot of Pets", name) 
 
-        ##### √√√√√ #####
+        ##### √√√√√ #####       Q2
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
 
-        ##### √√√√√ #####
+        ##### √√√√√ #####       Q3
     def test_add_or_remove_cash__add(self):
         add_or_remove_cash(self.cc_pet_shop,10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1010, cash)
-
+        
+        ##### √√√√√ #####       Q4
     def test_add_or_remove_cash__remove(self):
         add_or_remove_cash(self.cc_pet_shop,-10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(990, cash)
 
-    @unittest.skip("delete this line to run the test")
+        ##### √√√√√ #####       Q5
     def test_pets_sold(self):
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
+        ##### √√√√√ #####       Q6
     def test_increase_pets_sold(self):
         increase_pets_sold(self.cc_pet_shop,2)
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
+        ##### √√√√√ #####       Q7
     def test_stock_count(self):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
 
-    @unittest.skip("delete this line to run the test")
+        ##### √√√√√ #####       Q8
     def test_all_pets_by_breed__found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
         self.assertEqual(2, len(pets))
